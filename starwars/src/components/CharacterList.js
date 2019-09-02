0e90aae107e4101ac4e1a4a9c16612dc051e55fe
Raul Ortiz <raulortizars@gmail.com>
2019-09-01 18:35:19 -0600

    2

---
+++ starwars/src/components/CharacterList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const List = styled.div`
+width: 800px;
+margin: 0 auto;
+display: flex;
+flex-direction: column;
+justify content: space-evenly;`;
+
+
+export default function CharacterList(props) {
+
+    return (
+        <List></List>
+    )
+}
