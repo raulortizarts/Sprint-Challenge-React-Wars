d20770cb4aa0e7fbc7c87e46e0d655e0a4f495a5
Raul Ortiz <raulortizars@gmail.com>
2019-09-01 19:06:10 -0600

    4

--- starwars/src/components/CharacterList.js
+++ starwars/src/components/CharacterList.js
@@ -4,17 +4,37 @@ import styled from 'styled-components';
 const List = styled.div`
 width: 800px;
 margin: 0 auto;
+background: rgb(47,47,47, 0.3);
+border-radius: 20px;
+box-shadow: 5px 6px 5px;
 display: flex;
 flex-direction: column;
 justify content: space-evenly;`;
 
+const Title = styled.h1`
+color: white;
+margin: 10px;
+text-align: left;
+`;
+
+const Name = styled.h2`
+color: white;
+`;
 
 export default function CharacterList(props) {
 
     return (
         <List>
+            <Title>Multi Pass</Title>
+            <Name>{props.people.name}</Name>
+            {props.people.birth_year}
+            {props.people.species}
+            {props.people.homeworld}
+            {props.people.gender}
+            {props.people.skin_color}
+            {props.people.eye_color}
+            {props.people.hair_color}
 
-            {props.people.name}
         </List>
     )
 }
